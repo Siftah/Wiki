34cc10589674910460e054c3a2f8e3366c1bbf91
John Baxendale <John@siftah.com>
2014-05-07 11:39:22 +0200

    Updated code to be more generic and include additional examples.

--- HAHub.ino
+++ HAHub.ino
@@ -1,13 +1,17 @@
+/*
+   Please refer to http://wiki.siftah.com/Arduino_433mhz_RF_Network_Controller for details
+   on this code and it's usage.
+*/
+
 #include <EtherCard.h>
 #include <IPAddress.h>
 #include <RCSwitch.h>
-
 #define MAX_STRING_LEN  25
 
 static byte mymac[] = { 0x00,0x24,0xE8,0xEB,0xEF,0x9D };
 static byte myip[] = { 192,168,1,99 };
 static byte gwip[] = { 192,168,1,1 };
-byte Ethernet::buffer[100]; // tcp/ip send and receive buffer
+byte Ethernet::buffer[50]; // tcp/ip send and receive buffer
 
 RCSwitch mySwitch = RCSwitch();
 
@@ -22,18 +26,37 @@ void ethernetHandler(word port, byte ip[4], const char *data, word len) {
   Serial.print("' | Value: '");
   Serial.print(value);
   Serial.println("'");
-  if (strcmp(command,"heat")==0){
-  if (strcmp(value,"0")==0){
-    //turn heating off
-    Serial.println("Turning heating Off");
-    mySwitch.sendTriState("FFFFFFFF0010");
-  } else {
-    //turn heating on
-    Serial.println("Turning heating On");
-    mySwitch.sendTriState("FFFFFFFF0001");
-
+  
+  if (strcmp(command,"heat")==0){ 
+    mySwitch.setPulseLength(125);   // RFRemotech Relays
+    if (strcmp(value,"0")==0){
+      mySwitch.sendTriState("FFFFFFFF0010");
+    } else {
+      mySwitch.sendTriState("FFFFFFFF0001");
   }
 }
+
+  if (strcmp(command,"fan")==0){
+    mySwitch.setProtocol(1);
+    if (strcmp(value,"0")==0){
+        mySwitch.send("001100111110101101110111");
+    } else {
+        mySwitch.send("001100111110101101111111");
+    }
+  }
+
+/////// DUPLICATE FROM HERE //////////////////////////////
+
+  if (strcmp(command,"d")==0){ // change 'd' to the variable of your choice.
+    if (strcmp(value,"0")==0){ // if the values 0, turn off.
+        mySwitch.switchOff("11111","00010"); // amend to suit.
+    } else {
+        mySwitch.switchOn("11111","00010"); // turn switch on by default
+    }
+  }
+/////// TO HERE //////////////////////////////////////////
+
+
 }
 
 // Function to return a substring defined by a delimiter at an index
@@ -52,13 +75,8 @@ char* subStr (char* str, char *delim, int index) {
 
 void setup() {
   Serial.begin(9600);
-  // Transmitter is connected to Arduino Pin #10  
   mySwitch.enableTransmit(10);
-  // Optional set protocol (default is 1, will work for most outlets)
   mySwitch.setProtocol(1);
-  // Optional set pulse length.
-  mySwitch.setPulseLength(125); //130
-  // Optional set number of transmission repetitions.
   mySwitch.setRepeatTransmit(10);
   
     if (ether.begin(sizeof Ethernet::buffer, mymac, 10) == 0){
@@ -69,7 +87,6 @@ void setup() {
   ether.staticSetup(myip, gwip);
   ether.printIp("IP:  ", ether.myip);
   ether.udpServerListenOnPort(&ethernetHandler, 1337);
-  
 }
 
 void loop() {
